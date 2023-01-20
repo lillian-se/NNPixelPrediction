254b4d8e4eae9d966ea64d44c06c04f0894bec03
Lillian Brevik <77015337+lillian-se@users.noreply.github.com>
2023-01-20 07:21:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function setup() {
   var options = {
     inputs: [64, 64, 4],
     task: 'imageClassification',
-    debug: true,
+    debug: false,
   };
   pixelBrain = ml5.neuralNetwork(options);
 
